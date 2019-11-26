98f206117516f751630bf900f2925691bbb7e789
gregoire masson <g.masson279@gmail.com>
2019-11-26 17:12:58 +0100

    routing avancée

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
 {
     /**
-     * @Route("wild/show/{slug}", name="wild_show_index")
+     * @Route("/", name="wild_show_index")
      */
 
     public function index()
@@ -17,5 +18,21 @@ class WildController extends AbstractController
         return $this->render('wild/index.html.twig', [
             'website' => 'Bienvenue sur Wild Series',
         ]);
+    }
+    /**
+     * @Route("/wild/show/{slug}",
+     * requirements={"slug"="[a-z0-9-]+"},
+     * defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+     * name="wild_show")
+     */
+    public function show(string $slug): Response
+    {
+        if (preg_match_all("(-)", $slug)) {
+            $slug = ucwords(str_replace('-', ' ', $slug));
+        }
+        // redirection vers la page erreur, correspondant à l'insertion de majuscule dans l'URL
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug]);
+
     }
 }
